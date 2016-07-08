721cfc6746e34504c87b80b603c7af473369a6e4
Gregory Jefferis <jefferis@gmail.com>
2016-07-08 18:21:19 +0100

    add package docs
    * and import depends packages

---
+++ R/catnat-package.R
@@ -0,0 +1,9 @@
+#' Alex Bates' functions for use with nat and rcatmaid
+#'
+#' @name catnat-package
+#' @aliases catnat
+#' @docType package
+#' @keywords package
+#' @import nat
+#' @import catmaid
+NULL
