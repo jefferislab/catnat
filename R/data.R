f74593266d61157b4ba3571cdcf3c0da1939bbf4
alexanderbates <alexander.shakeel.bates@gmail.com>
2017-03-14 17:21:25 +0000

    * Added the LHN FC and dye-fill dataset with doumentation in "data/"

---
+++ R/data.R
@@ -0,0 +1,19 @@
+#' A comprehensive lateral horn neuron dataset
+#'
+#' A dataset containing all of the FlyCircuit lateral horn neurons that seem to have dendrite in the LH, as well as traced dye-fills from Fretcher et al. 2017
+#'
+#' @source \url{http://www.flycircuit.tw/}
+"most.lhns"
+
+#' A comprehensive lateral horn neuron dotprops dataset
+#'
+#' A dotprops dataset containing all of the FlyCircuit lateral horn neurons that seem to have dendrite in the LH, as well as traced dye-fills from Fretcher et al. 2017
+#'
+#' @source \url{http://www.flycircuit.tw/}
+"most.lhns.dps"
+
+#' Lateral horn primary neurite tracts
+#'
+#' The known primary neurite tracts that laterla horn neurons can occupy
+#'
+"primary.neurite.tracts"
