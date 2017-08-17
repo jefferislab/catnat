cfd3e046196053e526e58793c47d807f2fa2bf80
Gregory Jefferis <jefferis@gmail.com>
2017-08-17 19:30:14 +0100

    no need for catnat:::

--- R/prune.R
+++ R/prune.R
@@ -111,7 +111,7 @@ prune_online <-function(x, ...) UseMethod("prune_online")
 prune_online.neuron <- function(x, ...){
   continue = "no"
   while(!continue%in%c("y","yes")){
-    selected = catnat:::select.points(nat::xyzmatrix(x), plot3d = x)
+    selected = select.points(nat::xyzmatrix(x), plot3d = x)
     neuron = nat::prune(x, target = selected, keep = "near", maxdist = 0)
     rgl::plot3d(neuron, col ="black")
     continue = readline("Finished with this neuron? yes/no ")
