94912bc515300138b2e86c23b077aee580031964
alexanderbates <alexander.shakeel.bates@gmail.com>
2018-04-03 23:10:27 +0100

    Created in_neuropil function
    * measures cable inside given neuropil

--- R/inside_neuropils.R
+++ R/inside_neuropils.R
@@ -20,32 +20,20 @@ cable_inside_neuropils.neuron <- function(x, brain = nat.flybrains::FCWBNP.surf,
   if(!requireNamespace('nat.flybrains', quietly = TRUE))
     stop("You must install suggested package nat.flybrains to use this function!")
   method = method[1]
-  targets = c(0:100)
+  targets = c(-100:100)
   if (method=="axons"){targets = c(-2,2)}
   if (method=="dendrites"){targets = c(-3,3)}
   endings <- function(x){
     points=x$d[nat::endpoints(x)[which(endpoints(x)!=nat::rootpoints(x))],]
     nat::xyzmatrix(points[points$Label%in%targets,])
   }
-  in.neuropil <- function(x,neuropil,min.endpoints =1,alpha=30){
-    neuropil = alphashape3d::ashape3d(xyzmatrix(neuropil),alpha=alpha)
-    if(sum(alphashape3d::inashape3d(points=endings(x),as3d=neuropil, indexAlpha = "ALL"))>min.endpoints){
-      points = x$d[x$d$Label%in%targets,]
-      points.in = points[alphashape3d::inashape3d(points=nat::xyzmatrix(points),as3d=neuropil, indexAlpha = "ALL"),]
-      v = rownames(points.in)
-      pruned = tryCatch(nat::prune_vertices(x,verticestoprune=v,invert = TRUE),error = function(e)NULL)
-      if(!is.null(pruned)){
-        summary(pruned)$cable.length
-      }else{0}
-    }else{0}
-  }
-  sapply(brain$RegionList, function(n) in.neuropil(x=x,neuropil=subset(brain, n),min.endpoints=min.endpoints,alpha=alpha))
+  sapply(brain$RegionList, function(n) in_neuropil(x=x,brain = brain,neuropil=n,min.endpoints=min.endpoints,alpha=alpha))
 }
 
 #' @export
 #' @rdname cable_inside_neuropils
-cable_inside_neuropils.neuronlist <- function(x, brain = nat.flybrains::FCWBNP.surf, method = c("neurites","axons","dendrites"), min.endpoints = 2,alpha=30, ...){
-  nlapply(x, cable_inside_neuropils.neuron, brain=brain, method=method)
+cable_inside_neuropils.neuronlist <- function(x, brain = nat.flybrains::FCWBNP.surf, method = c("neurites","axons","dendrites"), min.endpoints = 1,alpha=30, ...){
+  nat::nlapply(x, cable_inside_neuropils.neuron, brain=brain, method=method)
 }
 
 #' @export
@@ -60,4 +48,30 @@ points_in_neuropil <- function(x, brain, alpha = 30, ...){
     df$neuropil[which(a==T)] = n
   }
   df
+}
+
+#' @export
+#' @rdname cable_inside_neuropils
+in_neuropil<-function(x,brain = nat.flybrains::FCWBNP.surf,neuropil = "LH_R",min.endpoints =1,alpha=alpha, ...) UseMethod("in_neuropil")
+
+#' @export
+#' @rdname cable_inside_neuropils
+in_neuropil.neuron <- function(x,brain = nat.flybrains::FCWBNP.surf,neuropil = "LH_R",min.endpoints =1,alpha=alpha){
+  neuropil = subset(brain,n)
+  neuropil = alphashape3d::ashape3d(nat::xyzmatrix(neuropil),alpha=alpha)
+  if(sum(alphashape3d::inashape3d(points=endings(x),as3d=neuropil, indexAlpha = "ALL"))>min.endpoints){
+    points = x$d[x$d$Label%in%targets,]
+    points.in = points[alphashape3d::inashape3d(points=nat::xyzmatrix(points),as3d=neuropil, indexAlpha = "ALL"),]
+    v = rownames(points.in)
+    pruned = tryCatch(nat::prune_vertices(x,verticestoprune=v,invert = TRUE),error = function(e)NULL)
+    if(!is.null(pruned)){
+      summary(pruned)$cable.length
+    }else{0}
+  }else{0}
+}
+
+#' @export
+#' @rdname cable_inside_neuropils
+in_neuropil.neuronlist <- function(x,brain = nat.flybrains::FCWBNP.surf,neuropil = "LH_R",min.endpoints =1,alpha=alpha){
+  nat::nlapply(x, in_neuropil.neuron, brain=brain, neuropil=neuropil,min.endpoints=min.endpoints,alpha=alpha)
 }
