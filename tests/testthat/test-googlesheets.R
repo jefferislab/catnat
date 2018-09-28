6abf98de24172e251953c0bf91a3eca4ed06081f
Gregory Jefferis <jefferis@gmail.com>
2018-09-28 10:26:25 +0100

    update test baseline v13->v14

--- tests/testthat/test-googlesheets.R
+++ tests/testthat/test-googlesheets.R
@@ -13,8 +13,8 @@ test_that("construct connector URL", {
     class = c("tbl_df", "tbl", "data.frame")
   )
 
-  testouput=c("https://neuropil.janelia.org/tracing/fafb/v13?pid=1&zp=167930&yp=235265&xp=434086&tool=tracingtool&active_skeleton_id=39254&sid0=5&s0=0",
-    "https://neuropil.janelia.org/tracing/fafb/v13?pid=1&zp=153790&yp=238586&xp=439796&tool=tracingtool&active_skeleton_id=1316642&sid0=5&s0=0")
+  testouput=c("https://neuropil.janelia.org/tracing/fafb/v14?pid=1&zp=167930&yp=235265&xp=434086&tool=tracingtool&active_skeleton_id=39254&sid0=5&s0=0",
+    "https://neuropil.janelia.org/tracing/fafb/v14?pid=1&zp=153790&yp=238586&xp=439796&tool=tracingtool&active_skeleton_id=1316642&sid0=5&s0=0")
   expect_equal(connector_URL(testinput), testouput)
 
   expect_error(apply(testinput, 1, connector_URL))
