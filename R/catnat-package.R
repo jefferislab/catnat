440cfe6a2292ab5ae441ede336d766be9e9caa65
Gregory Jefferis <jefferis@gmail.com>
2019-03-20 13:52:38 +0000

    Autogenerated package docs (with web links)

--- R/catnat-package.R
+++ R/catnat-package.R
@@ -1,13 +1,12 @@
-#' Alex Bates' functions for use with nat and rcatmaid
+#' @section Online Help: See \url{https://jefferislab.github.io/catnat} for
+#'   online documentation.
 #'
-#' @name catnat-package
+#' @keywords internal
 #' @aliases catnat
-#' @docType package
-#' @keywords package
 #' @import nat
 #' @import catmaid
 #' @import alphashape3d
 #' @import nat.nblast
 #' @import rgl
 #' @import nat.flybrains
-NULL
+"_PACKAGE"
